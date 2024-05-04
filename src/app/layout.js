e3b777c14b9cee83bc31447982269f6d7a9a1d8b
Zaenal Abidin <zaenal@kertasguru.com>
2024-05-04 10:53:21 +0800

    menu

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,10 @@ export const metadata = {
 function Navigation() {
   return (
     <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-      <a className="me-3 py-2 link-body-emphasis text-decoration-none" href="/dasboard">Dashboard</a>
+      <a className="me-3 py-2 link-body-emphasis text-decoration-none" href="/dashboard">Dashboard</a>
       <a className="me-3 py-2 link-body-emphasis text-decoration-none" href="/profile">Profile</a>
       <a className="me-3 py-2 link-body-emphasis text-decoration-none" href="/about">About</a>
+      <a className="me-3 py-2 link-body-emphasis text-decoration-none" href="/counter">Counter</a>
       <a className="py-2 link-body-emphasis text-decoration-none" href="/login">Login</a>
     </nav>
   );
