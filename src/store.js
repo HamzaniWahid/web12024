1d067389965f116ec251558cf0d2b5950f0ce46f
Zaenal <zaenal@getsurvey.id>
2024-05-17 20:48:14 +0800

    remove

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 // src/store.js
-import { configureStore } from '@reduxjs/toolkit';
-import loginReducer from './slices/loginSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import loginReducer from "./slices/loginSlice";
+import counterSlice from "./slices/counterSlice";
 export const store = configureStore({
   reducer: {
+    counter: counterSlice,
     login: loginReducer,
   },
 });
