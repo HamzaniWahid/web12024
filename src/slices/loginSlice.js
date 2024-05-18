5d1991d93af2f0cf05522670e071ebdbed792ae5
Zaenal Abidin <zaenal@kertasguru.com>
2024-05-18 10:33:55 +0800

    login to db

--- src/slices/loginSlice.js
+++ src/slices/loginSlice.js
@@ -1,5 +1,18 @@
 // slices/counterSlice.js
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const response = await axios.post('https:///login', credentials);
+      return response.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
 
 const loginSlice = createSlice({
   name: "login",
@@ -10,24 +23,25 @@ const loginSlice = createSlice({
     isLogin: false,
   },
   reducers: {
-    login: (state, actions) => {
-      let email = actions.payload.username;
-      let password = actions.payload.password;
-      if (!email || !password) {
-        state.errorMessage = "Please enter email and password";
-        return;
-      } else if (email != "admin@getsurvey.id" || password != "123") {
-        state.errorMessage = "Email atau password salah";
-      } else {
-        state.username = actions.payload.username;
-        state.isLogin = true;
-      }
-    },
     logout: (state, actions) => {
       state.isLogin = false;
     }
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.status = 'failed';
+        state.errorMessage = action.payload.message;
+      });
+  }
 });
 
-export const { login, logout } = loginSlice.actions;
+export const { logout } = loginSlice.actions;
 export default loginSlice.reducer;
