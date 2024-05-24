b6d836169f79ad1f0e8855cfe12d04d93d077da0
Zaenal <zaenal@getsurvey.id>
2024-05-24 14:42:16 +0800

    menu mahasiswa

--- src/components/Navigations.js
+++ src/components/Navigations.js
@@ -14,6 +14,7 @@ function Navigation() {
   return (
     <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
       <a className="me-3 py-2 link-body-emphasis text-decoration-none" href="/dashboard">Dashboard</a>
+      <a className="me-3 py-2 link-body-emphasis text-decoration-none" href="/mahasiswa">Mahasiswa</a>
       <a className="me-3 py-2 link-body-emphasis text-decoration-none" href="/profile">Profile</a>
       <a className="me-3 py-2 link-body-emphasis text-decoration-none" href="/about">About</a>
       <a className="me-3 py-2 link-body-emphasis text-decoration-none" href="/counter">Counter</a>
